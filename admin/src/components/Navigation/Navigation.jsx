eb9d173853d7dc2e457e460126525a9437065bcd
Keston-Antonio-Schmidtgen <keston.antonio.schmidtgen@gmail.com>
2022-03-27 19:26:58 +0200

    Update

--- admin/src/components/Navigation/Navigation.jsx
+++ admin/src/components/Navigation/Navigation.jsx
@@ -1,53 +1,76 @@
-import React, { useState } from "react";
+/* import React, { useState } from "react"; */
 import {
   MDBNavbar,
-  MDBContainer,
-  MDBIcon,
   MDBNavbarNav,
   MDBNavbarItem,
-  MDBNavbarLink,
   MDBNavbarToggler,
-  MDBNavbarBrand,
-  MDBCollapse,
-} from "mdb-react-ui-kit";
+  MDBNavbarLink,
+  MDBContainer,
+  MDBIcon 
+} from 'mdb-react-ui-kit';
 
 export default function Navigation() {
-  const [showNavColor, setShowNavColor] = useState(false);
-  return (
-    <>
-      <MDBNavbar expand="lg" dark bgColor="dark">
+  /* const [showNavColor, setShowNavColor] = useState(false); */
+  
+    return (
+      <header>
+      <MDBNavbar expand='lg' light bgColor='white' sticky>
         <MDBContainer fluid>
-          <MDBNavbarBrand to="#">Navbar</MDBNavbarBrand>
           <MDBNavbarToggler
-            type="button"
-            data-target="#navbarColor02"
-            aria-controls="navbarColor02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={() => setShowNavColor(!showNavColor)}
+            aria-controls='navbarExample01'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <MDBIcon icon="bars" fas />
+            <MDBIcon fas icon='bars' />
           </MDBNavbarToggler>
-          <MDBCollapse show={showNavColor} navbar id="navbarColor02">
-            <MDBNavbarNav className="me-auto mb-2 mb-lg-0">
-              <MDBNavbarItem className="active">
-                <MDBNavbarLink aria-current="page" to="/">
+          <div className='collapse navbar-collapse' id='navbarExample01'>
+            <MDBNavbarNav right className='mb-2 mb-lg-0'>
+              <MDBNavbarItem active>
+                <MDBNavbarLink aria-current='page' href='#'>
                   Home
                 </MDBNavbarLink>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink to="#">Features</MDBNavbarLink>
+                <MDBNavbarLink href='#'>Prices</MDBNavbarLink>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink to="#">Pricing</MDBNavbarLink>
+                <MDBNavbarLink href='#'>Blog</MDBNavbarLink>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink to="#">About</MDBNavbarLink>
+                <MDBNavbarLink href='#'>Contact us</MDBNavbarLink>
               </MDBNavbarItem>
             </MDBNavbarNav>
-          </MDBCollapse>
+          </div>
         </MDBContainer>
       </MDBNavbar>
-    </>
+  
+        <div className='mask' style={{ backgroundColor: '#FBFBFB' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-black'>
+              <h1 className='mb-3'>Sea To Sky</h1>
+              <h4 className='mb-4 '> Cool Stuff</h4>
+              <a
+                className='btn btn-outline-black btn-lg m-2'
+                href='http://localhost:3005/login'
+                role='button'
+                rel='nofollow'
+                target='_blank'
+              >
+                Explore
+              </a>
+              <a
+                className='btn btn-outline-black btn-lg m-2'
+                href='http://localhost:3005/register'
+                target='_blank'
+                role='button'
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+    </header>
   );
-}
+} 
+
+
