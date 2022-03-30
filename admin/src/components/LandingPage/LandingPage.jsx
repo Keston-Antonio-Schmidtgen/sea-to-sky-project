a9538802142a1e62a1f7852de089a1a47e887e64
Keston Antonio Schmidtgen <84077358+Keston-Antonio-Schmidtgen@users.noreply.github.com>
2022-03-30 11:05:11 +0200

    Update LandingPage.jsx

--- admin/src/components/LandingPage/LandingPage.jsx
+++ admin/src/components/LandingPage/LandingPage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 
-export default function Home() {
+export default function LandingPage() {
   return (
     <div>
-      <h1>Landing page</h1>
-      <Navigation />
-      <button>Explore (goes down and explores)</button>
-      <Link to="/register">Get Started</Link>
+      {/* <h1>Landing page</h1> */}
+     { <Navigation />}
+      {/* <button>Explore (goes down and explores)</button> */}
+      {/* <Link to="/register">Get Started</Link> */}
     </div>
   );
 }
