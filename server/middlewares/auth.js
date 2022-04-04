5069149600a4594f733f2b707579b0e7efd18585
Simona <semuona@gmail.com>
2022-04-04 16:42:56 +0200

    tags added

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -4,14 +4,14 @@ module.exports = (req, res, next) => {
   // examine if there is a token
   try {
     const token = req.cookies.cookiename;
-    console.log("AUTH: token is", token);
+    //  console.log("AUTH: token is", token);
 
     if (!token) return res.status(400).send({ success: false });
 
     const decodedToken = jwt.verify(token, process.env.SECRET);
-    console.log("decodedTOken is", decodedToken);
+    // console.log("decodedTOken is", decodedToken);
 
-    console.log("expiration date is", new Date(decodedToken.exp * 1000));
+    // console.log("expiration date is", new Date(decodedToken.exp * 1000));
 
     if (!decodedToken) return res.status(400).send({ success: false });
 
