c7f546552134b6401ee74a337791de5a851cde8b
esrkkn <esrakkan.ea@gmail.com>
2022-04-04 11:35:53 +0200

    ""

--- admin/src/components/Admin/Admin.jsx
+++ admin/src/components/Admin/Admin.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { WordContext } from "../context";
 import { useHistory, Link } from "react-router-dom";
 import { useContext } from "react";
+import SideNav from "../SideNav/sidenav"
 
 export default function Admin() {
   const { currentAdmin, setCurrentAdmin } = useContext(WordContext);
@@ -15,20 +16,12 @@ export default function Admin() {
   console.log("currentAdmin is from admin", currentAdmin);
   return (
     <div>
-      {" "}
+      <SideNav />
       <button onClick={handleLogout}>LogOut</button>
-      <h1> posts and blogs been created</h1>
-      <div>
-        <button>Home</button>
-        <button>Posts</button>
         <Link to="/allPosts">To All Posts</Link>
         <Link to="/addPost">|To Add Post|</Link>
-        <button>Pages</button>
-        <button>Category</button>
-        <button>Comments</button>
-        <button>Users</button>
-      </div>
-            <Sidenav />
+        
+            
 
     </div>
   );
