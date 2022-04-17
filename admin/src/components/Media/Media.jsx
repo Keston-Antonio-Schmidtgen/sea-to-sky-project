3f653c18b5d2529ddd2700e396840a526b3523e3
esrkkn <esrakkan.ea@gmail.com>
2022-04-17 15:31:18 +0200

    Merge branch 'main' of github.com:Keston-Antonio-Schmidtgen/sea-to-sky-project

--- admin/src/components/Media/Media.jsx
+++ admin/src/components/Media/Media.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Media.scss"
+import "./Media.scss";
 export default function Media({ toggleClass }) {
   const [newImage, setNewImage] = useState("");
   const { currentAdmin } = useContext(WordContext);
@@ -22,7 +22,6 @@ export default function Media({ toggleClass }) {
     Object.entries(data).forEach((item) => formdata.set(item[0], item[1]));
 
     try {
-
       if (newImage) formdata.set("image", newImage, "image");
 
       const response = await axios.post("/media/add", formdata);
@@ -30,31 +29,28 @@ export default function Media({ toggleClass }) {
 
       if (response.data.success)
         // setData({ ...data, image: [...data.image, newImage] });
-        setNewImage([...newImage, response.data.image])
-        console.log("image 2 is", newImage);
+        setNewImage(response.data.image);
+      console.log("image 2 is", newImage);
 
     } catch (error) {
-
       console.log(error.message);
     }
 
     console.log("Image is:", newImage);
   };
 
-  useEffect( () => {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("media/list");
 
-  const getData = async () => {
+      console.log("response is", response);
 
-    const response = await axios.get('media/list')
+      setImages([...response.data]);
+    };
+    getData();
+    console.log("images", images);
+  }, [newImage]);
 
-    console.log('response is', response);
-
-    setImages([...response.data])
-  }
-  getData()
-  console.log("images", images );
-
-}, [])
   return (
     <div className={`${toggleClass} container`}>
       <h2>Media</h2>
@@ -75,14 +71,13 @@ export default function Media({ toggleClass }) {
           <Button variant="info" type="submit" onClick={handleImage}>
             Add new
           </Button>
-          <section style={{display: "flex" , flexWrap: "wrap"}}>
-    {images.map((item) => (
-      
-        <div key={item._id} >
-          <img src={item.image} alt="" />
-        </div>
-        ))}
-  </section>
+          <section style={{ display: "flex", flexWrap: "wrap" }}>
+            {images.map((item) => (
+              <div key={item._id}>
+                <img src={item.image} alt="" />
+              </div>
+            ))}
+          </section>
         </div>
       </Form>
     </div>
