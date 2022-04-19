0a2d3a5900e22ae42b40df0d31823c9476f379b6
Simona <semuona@gmail.com>
2022-04-19 12:29:15 +0200

    right side nav done

--- admin/src/components/Posts/AddPost.jsx
+++ admin/src/components/Posts/AddPost.jsx
@@ -34,32 +34,6 @@ export default function AddPost({ toggleClass, previewPost }) {
   }, [data]);
 
   const editorRef = useRef(null);
-
-  /*   const handleSave = async () => {
-    setEmpty(!empty);
-    if (editorRef.current.getContent()) {
-      console.log(
-        "editorRef.current.getContent is:",
-        editorRef.current.getContent()
-      );
-
-      console.log("data is", data);
-
-      const response = await axios.post("/posts/add", data);
-
-      console.log("response is from add post", response);
-      setData({
-        body: "",
-        title: "",
-        subtitle: "",
-        published: false,
-        tags: [],
-        categories: [],
-        image: "",
-      });
-    }
-  };
- */
   const handlePublish = async () => {
     setEmpty(!empty);
     if (editorRef.current.getContent()) {
@@ -88,35 +62,10 @@ export default function AddPost({ toggleClass, previewPost }) {
 
   const handleEditorChange = () => {
     setData({ ...data, body: editorRef.current.getContent() });
-    // console.log("post is", data);
   };
 
-  /*   const handleTagSubmit = (e) => {
-    e.preventDefault();
-
-    console.log("this is form submit");
-    // add Tag to the data object at property tags.
-    setData({ ...data, tags: [...data.tags, tag] });
-    setTag("");
-  };
-  const handleDeleteTag = (idx) => {
-    const oldData = { ...data };
-    data.tags.splice(idx, 1);
-
-    setData({ ...oldData });
-  }; */
   return (
     <div className={`${toggleClass} container`}>
-      {/*       <input
-        placeholder="Type the title"
-        value={data.title}
-        onChange={(e) => setData({ ...data, title: e.target.value })}
-      />
-      <input
-        placeholder="Type the subtitle"
-        value={data.subtitle}
-        onChange={(e) => setData({ ...data, subtitle: e.target.value })}
-      /> */}
       <Box sx={{ "& > :not(style)": { m: 1 } }} className={toggleClass}>
         <Box sx={{ display: "flex", alignItems: "flex-end" }}>
           <TextField
@@ -159,13 +108,6 @@ export default function AddPost({ toggleClass, previewPost }) {
         }}
         onEditorChange={handleEditorChange}
       />
-
-      {/* <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Link to="/home">Home</Link>
-        <button onClick={handleSave}>Save</button>
-      </div> */}
-      {/* <Link to="/admin">Back to Home</Link> */}
-
       <SideNavRight
         title={data.title}
         body={data.body}
